56e8a34d1e8300a578772ef5d9001ddc67500bbb
Pechenka-ovsynka <66328619+Pechenka-ovsynka@users.noreply.github.com>
2020-06-07 15:00:22 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+#include "Program.h"
+//using namespace std;
+int main(int argc, char* argv[]) {
+	if(argc !=3) 
+		{
+		cout << "incorrect command line! "
+		"Waited: command infile outfile" << endl;
+		return(1);
+		}
+	ifstream ifst(argv[1]);
+	ofstream ofst(argv[2]);
+	cout << "Start"<< endl;
+	container* c;
+	 c= Init();
+	In(c, ifst);
+	 ofst << "Filled container. " << endl;
+	Out_only_first(c, ofst);
+	Clear(c);
+	 ofst << "Empty container. " << endl;
+	Out(c, ofst);
+	cout << "Stop"<< endl;
+	return 0;
+}
+
