49048c91e6d3762496ef08b166912b11abdc3495
Pechenka-ovsynka <66328619+Pechenka-ovsynka@users.noreply.github.com>
2020-06-14 00:18:33 +0700

    Add files via upload

---
+++ lab6_proc/main.cpp
@@ -0,0 +1,33 @@
+#include "Program.h"
+int main(int argc, char* argv[]) {
+	if(argc !=3) {
+		cout << "incorrect command line! "
+		"Waited: command infile outfile" << endl;
+		return(1);
+	}
+	ifstream ifst(argv[1]);
+	ofstream ofst(argv[2]);
+	//ifstream ifst("in.txt");
+	//ofstream ofst("ot.txt");
+	cout << "Start"<< endl;
+	container* c;
+	c= Init();
+	int tmp=1;
+	tmp=In(c, ifst);
+	if(tmp==0){
+		cout<<"incorrect input"<<endl;
+		return 0;
+	}
+	ofst << "Filled sorted container. " << endl;
+	Sort(c);
+	//Out(c, ofst);
+	Num_prep(c, ofst);
+	ofst << "Only aphorism " << endl;
+	Out_only_aphorism(c, ofst);
+	Clear(c);
+	ofst << "Empty container. " << endl;
+	Out(c, ofst);
+	cout << "Stop"<< endl;
+	return 0;
+}
+
