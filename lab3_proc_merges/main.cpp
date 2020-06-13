9479c5477131a0f1ef52a408554949a64db9dac3
Pechenka-ovsynka <66328619+Pechenka-ovsynka@users.noreply.github.com>
2020-06-13 19:35:35 +0700

    Add files via upload

---
+++ lab3_proc_merges/main.cpp
@@ -0,0 +1,29 @@
+
+#include "Program.h"
+//using namespace std;
+int main(int argc, char* argv[]) {
+	if(argc !=3) 
+		{
+		cout << "incorrect command line! "
+		"Waited: command infile outfile" << endl;
+		return(1);
+		}
+	ifstream ifst(argv[1]);
+	ofstream ofst(argv[2]);
+	cout << "Start"<< endl;
+	container* c;
+	c= Init();
+	In(c, ifst);
+	ofst << "Filled sorted container. " << endl;
+	Sort(c);
+	//Out(c, ofst);
+	num_prep(c, ofst);
+	ofst << "Only aphorism " << endl;
+	Out_only_aphorism(c, ofst);
+	Clear(c);
+	ofst << "Empty container. " << endl;
+	Out(c, ofst);
+	cout << "Stop"<< endl;
+	return 0;
+}
+
