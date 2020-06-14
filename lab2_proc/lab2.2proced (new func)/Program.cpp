32b347b0fc024f76d5a09803d05fff6e5d709900
Pechenka-ovsynka <66328619+Pechenka-ovsynka@users.noreply.github.com>
2020-06-14 22:34:07 +0700

    Add files via upload

---
+++ lab2_proc/lab2.2proced (new func)/Program.cpp
@@ -0,0 +1,165 @@
+#include "Program.h"
+using namespace std;
+	 
+struct container* In(container *lst,ifstream &ifst){
+		struct container *last = Init2(In(ifst));
+		struct container *tmp;
+		tmp = lst->next; 
+		lst->next = last; 
+		last->next = tmp; 	  
+	while(!ifst.eof()) {			
+		  struct container *newItem = Init2(In(ifst));
+		  struct container *tmp;		  
+		  tmp = last->next; 
+		  last->next = newItem;
+		  newItem->next = tmp;
+		  last=newItem;
+		}
+		return(last);
+	}
+ 	
+void In(aphorism &a, ifstream &ifst) {
+ char str[20];
+ ifst.get();
+ ifst.getline( a.x, sizeof(str)).eof();
+ ifst.getline(a.y, sizeof(str)).eof();
+ }
+ 
+  void In(poslov &p, ifstream &ifst){
+ 	char str[20];
+	ifst.get();
+ 	ifst.getline( p.a, sizeof(str)).eof();
+ 	ifst.getline(p.b, sizeof(str)).eof();
+ }
+ 
+ mudr* In(ifstream &ifst){
+ 	mudr *sp;
+ 	int k;
+ 	ifst >> k;
+ 	switch(k) {
+ 		case 1:
+ 			sp = new mudr;
+ 			sp->k = mudr::key::APHORISM;
+ 			In(sp->a, ifst);
+ 			return sp;
+ 		case 2:
+ 			sp = new mudr;
+ 			sp->k = mudr::key::POSLOV;
+ 			In(sp->p, ifst);
+ 			return sp;
+ 		default:
+ 		return 0;
+ 	}
+ }
+ 
+ void Out(poslov &p, ofstream &ofst){
+	ofst << "It is Poslovica: Strana = "
+	 << p.a << ", text = " << p.b << endl;
+ }
+ 
+  void Out(aphorism &a, ofstream &ofst) {
+ 	ofst << "It is Aphorism: Author = " << a.x
+	<< ", text = " << a.y << endl;
+ }
+ 
+ void Out(mudr &s, ofstream &ofst) {
+	 switch(s.k) {
+	 	case mudr::key::APHORISM:
+	 		Out(s.a, ofst);
+	 		break;
+	 	case mudr::key::POSLOV:
+	 		Out(s.p, ofst);
+	 		break;
+	 	default:
+	 		ofst << "Incorrect figure!" << endl;
+ 	};
+ 	
+ };
+ 
+ void Out(container *lst,ofstream &ofst){ 
+	if(lst->next==lst){
+		ofst<<"none"<<endl;
+		return;
+	}
+	struct container *p;
+	p = lst->next;
+	int num=0;
+	  do {
+	    num=num+1;
+	    p = p->next; 
+	  } while (p != lst); 
+	  ofst<<"Container contains " << num 	<< " elements." << endl;
+  	p = lst->next;
+	  do {
+	    mudr *s=p->cont;
+	    Out(*s,ofst);
+	    ofst<<"Знаков препинания = "<<num_prep_mudr(*s)<<endl;
+	    p = p->next; 
+	  } while (p != lst); 
+}
+
+struct container *  Init(){
+	struct container *c = new container;
+  c->next = c; 
+  return c;
+}
+struct container *  Init2(mudr  *a) {
+  struct container *c = new container;
+  c->cont = a;
+  c->next = c; 
+  return c;
+}
+
+ struct container * Clear(container *lst){
+	struct container *p;
+	p=lst->next;
+  do 
+  {
+	struct container *temp;
+	temp = p;
+	while (temp->next != p) { 
+			temp = temp->next;
+		}
+  	temp->next = p->next; 
+	p = p->next; // переход к следующему узлу
+  } while (p != lst); // условие окончания обхода
+}
+
+
+int num_prep_mudr(mudr &m){
+	switch(m.k){
+		case mudr::key::APHORISM:
+			return num_prep_aphorism(m.a);
+		case mudr::key::POSLOV:
+			return num_prep_poslov(m.p);
+		default:
+			return 0;
+	}
+}
+
+int num_prep_aphorism(aphorism &a){
+	int num=0;
+	for(int count=0;count<20;count++){
+		if (a.y[count]=='\0'){
+		break;}
+		if ((a.y[count]==',' ) || (a.y[count]=='.') || (a.y[count]=='!') || (a.y[count]=='?') || (a.y[count]==':') || (a.y[count]=='-') || (a.y[count]==';')){
+		num++;
+		}
+	}
+	return num;
+}
+int num_prep_poslov(poslov &p){
+	int num=0;
+	for(int count=0;count<20;count++){
+		if (p.b[count]=='\0'){
+		break;}
+		if ((p.b[count]==',' ) || (p.b[count]=='.') || (p.b[count]=='!') || (p.b[count]=='?') || (p.b[count]==':') || (p.b[count]=='-') || (p.b[count]==';')){
+		num++;
+		}
+	}
+	return num;
+}
+ 
+ 
+ 
+ 
---
+++ lab2_proc/lab2.4proced (sort)/main.cpp
@@ -0,0 +1,28 @@
+
+#include "Program.h"
+//using namespace std;
+int main(int argc, char* argv[]) {
+//	if(argc !=3) 
+//		{
+//		cout << "incorrect command line! "
+//		"Waited: command infile outfile" << endl;
+//		return(1);
+//		}
+//	ifstream ifst(argv[1]);
+//	ofstream ofst(argv[2]);
+	ifstream ifst("in.txt");
+	ofstream ofst("out.txt");	
+	cout << "Start"<< endl;
+	container* c;
+	 c= Init();
+	In(c, ifst);
+	Sort(c);
+	 ofst << "Filled container. " << endl;
+	Out(c, ofst);
+	Clear(c);
+	 ofst << "Empty container. " << endl;
+	Out(c, ofst);
+	cout << "Stop"<< endl;
+	return 0;
+}
+
